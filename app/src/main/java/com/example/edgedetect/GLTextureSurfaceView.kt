a070400d0ab7324a7483b7a892c78b21b1be800c
Pavani A <pavania200510@gmail.com>
2025-11-14 13:38:33 +0530

    Add GLTextureSurfaceView for OpenGL rendering

--- app/src/main/java/com/example/edgedetect/GLTextureSurfaceView.kt
+++ app/src/main/java/com/example/edgedetect/GLTextureSurfaceView.kt
@@ -1,1 +1,25 @@
+package com.example.edgedetect
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.opengl.GLSurfaceView
+import android.util.AttributeSet
+
+class GLTextureSurfaceView(context: Context, attrs: AttributeSet?) : GLSurfaceView(context, attrs) {
+    private val renderer: GLRenderer
+
+    constructor(context: Context) : this(context, null)
+
+    init {
+        setEGLContextClientVersion(2)
+        renderer = GLRenderer()
+        setRenderer(renderer)
+        renderMode = RENDERMODE_WHEN_DIRTY
+    }
+
+    fun queueFrame(bmp: Bitmap) {
+        renderer.updateBitmap(bmp)
+        requestRender()
+    }
+}
 
