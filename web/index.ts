b19894efd1f16f015cd7afefc1e90981bca84467
Pavani A <pavania200510@gmail.com>
2025-11-14 13:34:22 +0530

    Add FPS display functionality to index.ts

--- web/index.ts
+++ web/index.ts
@@ -1,1 +1,6 @@
-console.log('viewer');
+const img = document.getElementById('frame') as HTMLImageElement;
+const stats = document.getElementById('stats') as HTMLDivElement;
+let fps = 12;
+setInterval(() => {
+  stats.innerText = `FPS: ${fps}`;
+}, 500);
