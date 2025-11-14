fcd65f6a6c487b4557bb408f41bdaf30a892a4b5
Pavani A <pavania200510@gmail.com>
2025-11-14 13:13:01 +0530

    Set up gradlew.bat for Gradle wrapper
    Initialize Gradle wrapper script with necessary configurations.

--- gradlew.bat
+++ gradlew.bat
@@ -1,2 +1,10 @@
 @echo off
-echo Gradle wrapper placeholder
+set DIR=%~dp0
+set APP_HOME=%DIR%
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+"%JAVA_HOME%\bin\java.exe" %DEFAULT_JVM_OPTS% ^
+  -Dfile.encoding=UTF-8 ^
+  -classpath %CLASSPATH% ^
+  org.gradle.wrapper.GradleWrapperMain %*
